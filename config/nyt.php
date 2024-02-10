1ca41651580a56d6a1eb1aaee4e82ddc92bc9fe6
duca43 <dusan.maksic96@gmail.com>
2024-02-10 03:26:12 +0100

    Add config for NYT with data related to Books API

---
+++ config/nyt.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | New York Times Books API Base Url
+    |--------------------------------------------------------------------------
+    |
+    | Base url for Books API provided by NYT used to fetch information
+    | about book reviews and The New York Times Best Sellers lists.
+    |
+    */
+
+    'books_api_base_url' => env('NYT_BOOKS_API_BASE_URL', 'https://api.nytimes.com/svc/books/v3'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | New York Times Books API Key
+    |--------------------------------------------------------------------------
+    |
+    | Value used for API Key Authentication. When NYT Books API is being called,
+    | API key is provided as a query parameter named 'api-key'.
+    |
+    */
+
+    'books_api_key' => env('NYT_BOOKS_API_KEY'),
+];
