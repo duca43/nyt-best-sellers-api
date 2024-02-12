0a07e6dabb63b00e946011549c36e96a0ff6bc82
duca43 <dusan.maksic96@gmail.com>
2024-02-12 03:24:13 +0100

    Add controller with index method for fetching best sellers

---
+++ app/Http/Controllers/Nyt/BestSellersController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Nyt;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Nyt\BestSellersRequest;
+use App\Http\Resources\Nyt\BestSellerResource;
+use App\Services\Nyt\BestSellersService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+
+class BestSellersController extends Controller
+{
+    public function __construct(private BestSellersService $bestSellersService)
+    {
+    }
+
+    /**
+     * Retrieve the best sellers data and return it as a JSON response.
+     *
+     * @param  \App\Http\Requests\BestSellersRequest $request The incoming request containing the query parameters.
+     * @return \Illuminate\Http\JsonResponse Returns a JSON response containing the best sellers data or an error message.
+     */
+    public function index(BestSellersRequest $request): JsonResponse
+    {
+        $queryParams = $request->validated();
+
+        try {
+            $bestSellers = $this->bestSellersService->getBestSellers($queryParams);
+            return BestSellerResource::collection($bestSellers)->response();
+        } catch (TooManyRequestsHttpException $e) {
+            return response()->json(['message' => $e->getMessage()], 429);
+        } catch (BadRequestHttpException $e) {
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
+    }
+}
